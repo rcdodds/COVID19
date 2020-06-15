06a6c2fced946998681541ce25dee13b371d4ece
Ryan Dodds <46753908+rcdodds@users.noreply.github.com>
2020-06-15 01:07:24 -0400

    Add WSGI that is live on the website

---
+++ rcdodds_pythonanywhere_com_wsgi.py
@@ -0,0 +1,27 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
+import sys
+import os
+import time
+
+# set timezone to EST -- this prevents trying to pull data from a future day
+os.environ["TZ"] = "America/New_York"
+time.tzset()
+
+# add your project directory to the sys.path
+project_home = '/home/rcdodds/mysite'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# import flask app but need to call it "application" for WSGI to work
+from COVID_Website import app
+
+application=app.server
+
+if __name__ == "__main__":
+    application.run()
